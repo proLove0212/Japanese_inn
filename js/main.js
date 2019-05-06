a7c6fd208c4f2bf238a8631e1a2d3448acfb2ff5
PROLOVE <proLove0212@gmail.com>
2019-05-06 17:14:37 +0300

    Redistibuted files
    Redistirbuted pice of codes by files more rationatly. Now can work much more easer. It also fix bug with navigation dont't showing on screeen larger then 768 px. And make hamburger menu actualy work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 //// Js features
 
 $(function() {
+	/////Creacte mobile menu function////
+	$('.menu-toggle').click(function() {
 
+		$('.navbar').toggleClass('navbar--open');
+		$(this).toggleClass('open');
 
+	});
+
+	/*Give culture section "Load more" btn
+	function of loading more images after
+	is's clicked	
+ 	*/
+ 	$('#loadMore-btn').click(function(){
+
+ 		$('#events-row2').toggleClass('.events-item--btn', '.events-row2--showed');
+
+ 	});
+ 	////// It doesn't work yet 
 
 });
