5e465833238764403f474bce145af02e04b4cb68
PROLOVE <proLove0212@gmail.com>
2019-05-13 22:03:20 +0300

    Finish adaptive layout
    Now web page is full adaptive for any devises. Change breakpoint a littlebit. Create stylese for tablets and desktopt on pages. Conected links to the pages

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@
 
 $(function() {
 	/////Creacte mobile menu function////
+	////give menu-toggle event .click
+	//// when we click that humburger button
+	//// something happen
 	$('.menu-toggle').click(function() {
-
+		////something it's adding class navbar--open
+		//// to navbar by event toggleClass
+		//// also this class will be removed 
+		//// after second click that because 
+		//// toggleClass work that way 
+		//// in contrast with addClass
 		$('.navbar').toggleClass('navbar--open');
+		///// and by this we add clss open to parent class
+		//// of this function (this === '.menu-toggle')
 		$(this).toggleClass('open');
 
 	});
@@ -18,6 +28,8 @@ $(function() {
  		$('#events-row2').toggleClass('.events-item--btn', '.events-row2--showed');
 
  	});
- 	////// It doesn't work yet 
+ 	////// It doesn't work yet
 
+ 	
+ 	
 });
